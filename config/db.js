e80f36805354acd605e1c55d0aff7291730bd6b0
Yayi <yayinasir12@gmail.com>
2026-05-12 19:08:14 +0100

    mongoatlas  url live

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const localUrl = process.env.MONGO_URI_LOCAL;
+const liveUrl = process.env.MONGO_URI_LIVE;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(localUrl);
-    console.log("MongoDb connected locally");
+    await mongoose.connect(liveUrl);
+    console.log("MongoDb connected lively");
   } catch (error) {
     console.error(error);
     process.exit(1);
